06c12bd4246a3dd8f931827b7c895f330a410241
PatrycjaD <domanska.patrycja7@gmail.com>
2025-03-30 15:47:02 +0200

    fix(system): homepage route

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/balancing-market-app-web' element={<Home></Home>}></Route>
       </Routes>
     </Router>
   );
